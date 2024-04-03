41f4b2974213ed4b075ff21eb784e5eedf4fba1a
Zytorx <logins@zytorx.net>
2024-04-03 22:34:44 +0200

    Added UserGovPermissions Migrations

---
+++ database/migrations/2024_04_03_194058_create_user_gov_permissions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_gov_permissions', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('admin');
+            $table->boolean('auctions');
+            $table->boolean('houses');
+            $table->boolean('namechange');
+            $table->boolean('leader');
+            $table->boolean('economy');
+            $table->boolean('justice');
+            $table->boolean('state');
+            $table->boolean('secret_service');
+            $table->boolean('car_maintenance');
+            $table->boolean('registry_office');
+            $table->boolean('disabled');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('permissions');
+    }
+};
