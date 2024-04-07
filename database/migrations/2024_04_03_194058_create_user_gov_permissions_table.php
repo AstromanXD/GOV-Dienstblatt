7933081e1bbcdb547b71e9feac868fd97c4c3692
Zytorx <logins@d-bor.net>
2024-04-07 16:38:01 +0200

    Added a few migrations

--- database/migrations/2024_04_03_194058_create_user_gov_permissions_table.php
+++ database/migrations/2024_04_03_194058_create_user_gov_permissions_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('user_gov_permissions', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained('users');
             $table->boolean('admin');
             $table->boolean('auctions');
             $table->boolean('houses');
