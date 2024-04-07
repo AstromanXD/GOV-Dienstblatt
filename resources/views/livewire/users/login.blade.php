fcf13f820d65a9abf97608982e54083cfd5d9201
AstromanXD <81534117+AstromanXD@users.noreply.github.com>
2024-04-07 12:41:01 +0200

    Login push

---
+++ resources/views/livewire/users/login.blade.php
@@ -0,0 +1,13 @@
+<x-layout>
+    <div class="bg-gray-50 border border-gray-200 p-10 rounded max-w-lg mx-auto mt-24">
+        <header class="text-2xl font-bold, uppercase mb-1">Login</header>
+        <form action="">
+            <div class="mb-6">
+                <label for="name" class="inline-block text-lg mb-2"> E-Mail</label>
+                <label>
+                    <input type="text" class="border border-gray-200 rounded p-2 w-full" name="E-Mail">
+                </label>
+            </div>
+        </form>
+    </div>
+</x-layout>
